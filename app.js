75fa26d4213dc91b631f8c29d9040a6e26aba683
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-10-02 23:56:35 +0500

    new changes

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
 
 async function fetchweather() {
-    const res = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=london&appid=72226912565ed619f1d5661ec8b087a6&units=metric') 
-    const weatherdata = res.data;
-    return weatherdata;
-  
-    
-    // console.log(res.statusCode)
+  const res = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather?q=london&appid=72226912565ed619f1d5661ec8b087a6&units=metric"
+  );
+  const weatherdata = res.data;
+  return weatherdata;
 
-    // setMovieResponse(res.data.results)
+  // console.log(res.statusCode)
+
+  // setMovieResponse(res.data.results)
 }
 
-app.get("/", async function(req, res){
-   const response = await fetchweather(); 
-//    console.log(response);
-   const temp = response.main.temp;
-   const weatherdescription = response.weather[0].description;
-   const icon = response.weather[0].icon
-   const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//    console.log(temp); 
-res.write(`<p> The weather is currently ${weatherdescription}</p>`)
-   res.write(`<h1> the weather is london  ${temp}  degree calcius. </h1>`)
-   res.write(`<img src="${imageUrl}">`)
+app.get("/", async function (req, res) {
+  const response = await fetchweather();
+  //    console.log(response);
+  const temp = response.main.temp;
+  const weatherdescription = response.weather[0].description;
+  const icon = response.weather[0].icon;
+  const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+  //    console.log(temp);
+  res.write(`<p> The weather is currently ${weatherdescription}</p>`);
+  res.write(`<h1> the weather is london  ${temp}  degree calcius. </h1>`);
+  res.write(`<img src="${imageUrl}">`);
+  res.send();
+});
 
-} )
-
-
-app.listen(3000 , function(){
-    console.log("server is running on port 3000")
-})
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
