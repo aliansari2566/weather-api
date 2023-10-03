709ed075557fe3a86908251a4b2082b24c9914b7
Muhammad Ali Hamza <71075942+aliansari2566@users.noreply.github.com>
2023-10-03 12:50:09 +0500

    new changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ async function fetchweather() {
    const query ="london";
    const apikey = "72226912565ed619f1d5661ec8b087a6"; 
    const units = "metric";
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${query} &appid=${apikey}&units=${units}`
+   const url = `https://api.openweathermap.org/data/2.5/weather? q=${query} &appid=${apikey}&units=${units}`
   
    try {
       const res = await axios.get( url );
@@ -33,18 +33,29 @@ async function fetchweather() {
 }
 
 app.get("/", async function (req, res) {
-  const response = await fetchweather();
-  //    console.log(response);
-  const temp = response.main.temp;
-  const weatherdescription = response.weather[0].description;
-  const icon = response.weather[0].icon;
-  const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-  //    console.log(temp);
-  res.write(`<p> The weather is currently ${weatherdescription}</p>`);
-  res.write(`<h1> the weather in london is  ${temp}  degree calcius. </h1>`);
-  res.write(`<img src="${imageUrl}">`);
-  res.send();
+
+
+   res.sendFile(__dirname + "/index.html");
+
+
+
+//   const response = await fetchweather();
+//      console.log(response);
+//   const temp = response.main.temp;
+//   const weatherdescription = response.weather[0].description;
+//   const icon = response.weather[0].icon;
+//   const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+//      console.log(temp);
+//   res.write(`<p> The weather is currently ${weatherdescription}</p>`);
+//   res.write(`<h1> the weather in london is  ${temp}  degree calcius. </h1>`);
+//   res.write(`<img src="${imageUrl}">`);
+//   res.send();
 });
+
+app.post("/",function(req,res) {
+   console.log("post Request recieved");
+   
+})
 
 app.listen(3000, function () {
   console.log("server is running on port 3000");
